427a39cf20b3c956bda51b8477f760d440c4089b
Tawab Ahmed <109740218+TawabC223134@users.noreply.github.com>
2024-12-19 21:57:49 +0600

    Donor model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Donor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    contact = models.CharField(max_length= 11, null= True)
+    address = models.CharField(max_length= 1000, null= True)
+    userpic = models.ImageField(upload_to='donor', null= True, blank= True)
+    regdate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
